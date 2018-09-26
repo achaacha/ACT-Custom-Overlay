8d2c1656bd92d78cb6896be5377b5f0207357dfb
achaacha <43567591+achaacha@users.noreply.github.com>
2018-09-25 21:48:06 -0500

    Update mods.js

--- Acha'Acha Kagerou Custom/overlay/lib2/mods.js
+++ Acha'Acha Kagerou Custom/overlay/lib2/mods.js
@@ -1,9 +1,6 @@
 jQuery.noConflict();
-jQuery(document).ready(function($){
-	$("#p .general_box").hover(function() {
-		$("#p #profile_tabs").stop().fadeToggle();
-	})
-	$("#p .general_box").hover(function() {
-		$("#p .overview_half:last-child div").stop().fadeToggle();
-	})
+$(document).ready(function(){
+    $("body").hover(function(){
+        $("body").fadeToggle(100);
+    });
 });
