99a40ab78904ca85581335713972ea07746bebfb
achaacha <43567591+achaacha@users.noreply.github.com>
2018-09-27 05:03:22 -0500

    Update mods.js

--- Acha'Acha Kagerou Custom/overlay/lib/mods.js
+++ Acha'Acha Kagerou Custom/overlay/lib/mods.js
@@ -2,12 +2,7 @@ var jq = jQuery.noConflict();
 
 jq(document).ready(function(){
     jq("#init-menu > ul li.button").click(function(){
-        jq("nav").slideToggle(slow, function() {
-            if (jq(this).is(":visible")) {
-                 jq("#init-menu > ul li.button span").text('Hide');                
-            } else {
-                 jq("#init-menu > ul li.button span").text('Display');                
-            }        
+        jq("nav").slideToggle(); 
      });
             
  });
