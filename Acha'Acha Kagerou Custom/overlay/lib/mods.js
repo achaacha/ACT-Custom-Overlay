597572697ac8e5bda7e6efb2f3321fb1eedfd061
achaacha <43567591+achaacha@users.noreply.github.com>
2018-09-29 00:40:05 -0500

    Update mods.js

--- Acha'Acha Kagerou Custom/overlay/lib/mods.js
+++ Acha'Acha Kagerou Custom/overlay/lib/mods.js
@@ -10,7 +10,7 @@ jq(document).ready(function(){
      });
     
     jq(".class-pgl, .class-mnk, .class-lnc, .class-drg, .class-arc, .class-brd, .class-rog, .class-nin, .class-acn, .class-smn, .class-thm, .class-blm, .class-mch, .class-sam, .class-rdm, .class-gla, .class-pld, .class-mrd, .class-war, .class-drk, .class-gld, .class-cnj, .class-whm, .class-sch, .class-ast").hover(function(){
-        jq(".flex-column-deal-total, .flex-column-deal-per-second, .flex-column-deal-pct, .flex-column-etc-death").css('display','none !important')
+        jq(".flex-column-deal-total, .flex-column-deal-per-second, .flex-column-deal-pct, .flex-column-etc-death").css('display','none !important');
     });
    
             
