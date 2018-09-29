4f8abc0cfe125571a2822744aa6e3f5272d95693
achaacha <43567591+achaacha@users.noreply.github.com>
2018-09-28 21:16:37 -0500

    Update mods.js

--- Acha'Acha Kagerou Custom/overlay/lib/mods.js
+++ Acha'Acha Kagerou Custom/overlay/lib/mods.js
@@ -2,9 +2,7 @@ var jq = jQuery.noConflict();
 
 
 jq(document).ready(function(){
-    $("body").on('mouseenter mouseleave', function () {
-        $(this).fadeToggle(100);
-});
+
     
     jq("#button-list > ul li.button").click(function(){
         var navb = jq(this);
